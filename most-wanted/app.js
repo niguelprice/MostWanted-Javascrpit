3647ab7fb1772aedd5031c6ce89af82a35c962ed
Niguel Price <niguelprice@yahoo.com>
2022-10-31 00:28:24 -0400

    added more info to the display person function

--- most-wanted/app.js
+++ most-wanted/app.js
@@ -140,6 +140,14 @@ function displayPeople(people) {
 function displayPerson(person) {
     let personInfo = `First Name: ${person.firstName}\n`;
     personInfo += `Last Name: ${person.lastName}\n`;
+    personInfo += `Gender: ${person.gender}\n`;
+    personInfo += `DOB: ${person.dob}\n`;
+    personInfo += `Height: ${person.height}\n`;
+    personInfo += `Weight: ${person.weight}\n`;
+    personInfo += `EyeColor: ${person.eyeColor}\n`;
+    personInfo += `Occupation: ${person.occupation}\n`;
+    personInfo += `Parents: ${person.parents}\n`;
+    personInfo += `CurrentSpouse: ${person.currentSpouse}\n`;
     //! TODO #1a: finish getting the rest of the information to display //////////////////////////////////////////
     alert(personInfo);
 }
@@ -184,3 +192,15 @@ function chars(input) {
 
 //////////////////////////////////////////* End Of Starter Code *//////////////////////////////////////////
 // Any additional functions can be written below this line 👇. Happy Coding! 😁
+
+function searchByTraits(people) {
+    let occupation = promptFor("What occupation do they have?", chars);
+
+    
+    let foundPerson = people.filter(function (person) {
+        if (person.occupation === occupation) {
+            return true;
+        }
+    });
+    return foundPerson;
+}
